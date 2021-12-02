c31fd2f47fc9491be4e4e0beed36e56e755652be
manemejeff <kmb14056@yandex.ru>
2021-12-02 23:19:18 +0400

    Add profile template styling? dev info + skills

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from users.models import Profile
+
 
 def profiles(request):
-    return render(request, 'users/profiles.html')
+    profiles = Profile.objects.all()
+    context = {
+        'profiles': profiles,
+    }
+    return render(request, 'users/profiles.html', context)
