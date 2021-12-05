2d70ab2f928ddd14fb4776741cd137a1a5b99eb9
manemejeff <kmb14056@yandex.ru>
2021-12-05 23:16:09 +0400

    add profile with developer info, link to profile page from projects page (add and render profiles video)

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -10,3 +10,15 @@ def profiles(request):
         'profiles': profiles,
     }
     return render(request, 'users/profiles.html', context)
+
+def user_profile(request, pk):
+    profile = Profile.objects.get(id=pk)
+
+    top_skills = profile.skill_set.exclude(description__exact='')
+    other_skills = profile.skill_set.filter(description='')
+    context = {
+        'profile': profile,
+        'top_skills': top_skills,
+        'other_skills': other_skills,
+    }
+    return render(request, 'users/user_profile.html', context)
