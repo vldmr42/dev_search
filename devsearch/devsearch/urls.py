25379f9af621ba3218b4818bf414416676ab01d7
manemejeff <kmb14056@yandex.ru>
2021-12-21 20:08:50 +0400

    url fix (missed the 1 as_view)

--- devsearch/devsearch/urls.py
+++ devsearch/devsearch/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
 
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name='reset_password.html'),
          name='reset_password'),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView(template_name='reset_password_sent.html'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='reset_password_sent.html'),
          name='password_reset_done'),
     path('reset/<uid64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='reset.html'),
          name='password_reset_confirm'),
