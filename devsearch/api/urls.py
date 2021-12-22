c98b5bb241d09b38c6badb0f7ed0c1c7654a6bfb
manemejeff <kmb14056@yandex.ru>
2021-12-22 21:37:11 +0400

    added post request to create/modify review

--- devsearch/api/urls.py
+++ devsearch/api/urls.py
@@ -5,8 +5,6 @@ from rest_framework_simplejwt.views import (
 )
 from . import views
 
-
-
 urlpatterns = [
     path('users/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
@@ -14,4 +12,5 @@ urlpatterns = [
     path('', views.get_routes),
     path('projects/', views.get_projects),
     path('projects/<str:pk>/', views.get_project),
+    path('projects/<str:pk>/vote/', views.project_vote),
 ]
